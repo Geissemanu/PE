40dc10f0a9a5b1695624d17d2f73c902371241a4
Manuel Geissmann <79644604+Geissemanu@users.noreply.github.com>
2022-05-26 15:10:08 +0200

    3 a und b

---
+++ Unity code3A/Assets/Cube1.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+/*
+    Accelerates the cube to which it is attached, modelling an harmonic oscillator.
+    Writes the position, velocity and acceleration of the cube to a CSV file.
+    
+    Remark: For use in "Physics Engines" module at ZHAW, part of physics lab
+    Author: kemf
+    Version: 1.0
+*/
+public class Cube1 : MonoBehaviour
+{
+    public Rigidbody CubeOne;
+
+    public Rigidbody CubeTwo;
+
+    public int springConstant; // N/m
+
+    private float currentTimeStep; // s
+    
+    private List<List<float>> timeSeries;
+
+    public int springLength; // m
+    
+    private bool compress = false;
+
+    private bool expand = false;
+
+    private bool delay = false;
+
+    private bool locked = false;
+
+    private float previousDistance; // m
+
+    private float forceX = 0; // N
+
+    private float distance; // m
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSeries = new List<List<float>>();
+        
+        float startVelocity = 1f; // N
+
+        CubeOne.velocity = (new Vector3(startVelocity, 0f, 0f));
+        CubeTwo.velocity = (new Vector3(0f, 0f, 0f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate can be called multiple times per frame
+    void FixedUpdate() {
+
+        distance = Mathf.Abs(CubeOne.position.x) - 0.5f - (Mathf.Abs(CubeTwo.position.x) + 0.5f);
+        if (Mathf.Abs(CubeTwo.position.x) + 0.5f + springLength > Mathf.Abs(CubeOne.position.x) - 0.5f && expand != true && delay != true){
+            compress = true;
+        }
+
+        if (compress == true) {
+            apllySpringForce();
+            if (CubeOne.velocity.x <= CubeTwo.velocity.x){
+                compress = false;
+                expand = true;
+                locked = true;
+            }
+        }
+
+        if(CubeOne.position.x > 1){
+            locked = false;
+            delay = true;
+            apllySpringForce();
+        }
+
+        if (locked == true){
+            CubeOne.velocity = CubeTwo.velocity;
+        }
+
+        if (expand == true && delay == true && distance < springLength){
+            if (distance > previousDistance){
+                apllySpringForce();
+
+            }
+        }
+
+        currentTimeStep += Time.deltaTime;
+        timeSeries.Add(new List<float>() {currentTimeStep, CubeOne.velocity.x , CubeOne.position.x , -forceX, CubeTwo.velocity.x, CubeTwo.position.x, CubeTwo.velocity.x , distance});
+
+        previousDistance = distance;
+
+    }
+
+    void apllySpringForce(){
+        forceX = springConstant * (springLength - distance);
+        CubeOne.AddForce(new Vector3(-forceX, 0, 0));
+        CubeTwo.AddForce(new Vector3(forceX, 0, 0));
+    }
+
+    void OnApplicationQuit() {
+        WriteTimeSeriesToCSV();
+
+    }
+
+    void WriteTimeSeriesToCSV() {        
+        using (var streamWriter = new StreamWriter("time_series.csv")) {
+            streamWriter.WriteLine("t,v(t) [cube 1],x(t) [cube 1],F(t) [cube 1],v(t) [cube 2],x(t) [cube 2],F(t) [cube 2],distance");
+            
+            foreach (List<float> timeStep in timeSeries) {
+                streamWriter.WriteLine(string.Join(",", timeStep));
+                streamWriter.Flush();
+            }
+        }
+    }
+}
---
+++ Unity code3B/Assets/Cube1.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+/*
+    Accelerates the cube to which it is attached, modelling an harmonic oscillator.
+    Writes the position, velocity and acceleration of the cube to a CSV file.
+    
+    Remark: For use in "Physics Engines" module at ZHAW, part of physics lab
+    Author: kemf
+    Version: 1.0
+*/
+public class Cube1 : MonoBehaviour
+{
+    private Rigidbody CubeOne;
+
+    public int springConstant; // N/m
+
+    private float currentTimeStep; // s
+    
+    private List<List<float>> timeSeries;
+
+    public int springLength;
+    
+    private float previousDistance;
+
+    private float forceX = 0; // N
+
+    private float distance;
+
+    private bool turning;
+
+    private  const float RADIUS = 5.0f;
+    
+    private float oldVelocity;
+
+    private float deceleration;
+
+    private float forceC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeOne = GetComponent<Rigidbody>();
+        timeSeries = new List<List<float>>();
+        
+        float startVelocity = -1f; // N
+
+        CubeOne.velocity = (new Vector3(startVelocity, 0f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate can be called multiple times per frame
+    void FixedUpdate() {
+
+
+        if(CubeOne.position.x <= -2 && !turning) { 
+            turning = true;
+            //as this value has to be constant it is created here once.
+            deceleration = CubeOne.mass * CubeOne.velocity.sqrMagnitude/(RADIUS*(float)Math.PI);  //v^2/2*s => m*v^2/r*pi
+            oldVelocity = CubeOne.velocity.sqrMagnitude;
+            }
+        if(turning) {
+            if(CubeOne.velocity.sqrMagnitude > oldVelocity) {
+                CubeOne.velocity = Vector3.zero;
+                turning = false;
+            }
+
+            Vector3 F_deceleration = -CubeOne.velocity.normalized*deceleration;
+            forceC = CubeOne.velocity.sqrMagnitude/RADIUS * CubeOne.mass; //  centripetal force : v^2/r * m
+            Vector3 F_centripetal = Vector3.Cross(CubeOne.velocity.normalized, Vector3.up*forceC); // creates the needed Vector.
+
+            Vector3 F_res = F_centripetal + F_deceleration; // N
+            CubeOne.AddForce(F_res);
+            oldVelocity = CubeOne.velocity.sqrMagnitude;
+        }
+        //currentTimeStep += Time.deltaTime;
+        //timeSeries.Add(new List<float>() {currentTimeStep, CubeOne.position.x, CubeOne.velocity.x, forceX});
+    }
+
+    void OnApplicationQuit() {
+        WriteTimeSeriesToCSV();
+
+    }
+
+    void WriteTimeSeriesToCSV() {
+        using (var streamWriter = new StreamWriter("time_series.csv")) {
+            streamWriter.WriteLine("t,x(t),v(t),a(t) (added)");
+            
+            foreach (List<float> timeStep in timeSeries) {
+                streamWriter.WriteLine(string.Join(",", timeStep));
+                streamWriter.Flush();
+            }
+        }
+    }
+}
